0235fb29a815f281c8cd9e3da4a61f76f7f99d0a
lreichow <luca.reichow@uni-oldenburg.de>
2026-06-24 17:48:00 +0200

    fixed findById

--- src/repositories/participant.repository.js
+++ src/repositories/participant.repository.js
@@ -22,6 +22,14 @@ async function deleteSurvey(req, res) {
   res.json({ message: "deleted" });
 }
 
+async function findById(id) {
+  const rows = await db.query(`SELECT * FROM participants WHERE id = ?`, [id]);
+
+  return rows[0] || null;
+}
+
 module.exports = {
   create,
+  deleteSurvey,
+  findById,
 };
