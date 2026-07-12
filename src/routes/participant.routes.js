cc9aa383b2da5423165b8d3c69c9bb601bf3b941
lreichow <luca.reichow@uni-oldenburg.de>
2026-07-13 01:19:57 +0200

    added swagger api docs

--- src/routes/participant.routes.js
+++ src/routes/participant.routes.js
@@ -3,8 +3,47 @@ const router = express.Router();
 const { resolveParticipant } = require("../middleware/securityLayer");
 const controller = require("../controllers/participant.controller");
 
+/**
+ * @swagger
+ * /participants:
+ *   post:
+ *     summary: Create a new participant
+ *     tags:
+ *       - Participants
+ *     responses:
+ *       201:
+ *         description: Participant successfully created.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 participantId:
+ *                   type: string
+ *                   format: uuid
+ */
 router.post("/", controller.createParticipant);
 
+/**
+ * @swagger
+ * /participants/me:
+ *   get:
+ *     summary: Get current participant
+ *     tags:
+ *       - Participants
+ *     parameters:
+ *       - in: header
+ *         name: X-Participant-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       200:
+ *         description: Participant found.
+ *       404:
+ *         description: Participant not found.
+ */
 router.get("/me", resolveParticipant, controller.getParticipant);
 
 module.exports = router;
--- src/routes/survey.routes.js
+++ src/routes/survey.routes.js
@@ -3,6 +3,49 @@ const router = express.Router();
 const { resolveParticipant } = require("../middleware/securityLayer");
 const controller = require("../controllers/survey.controller");
 
+/**
+ * @swagger
+ * /surveys:
+ *   post:
+ *     summary: Submit a completed survey
+ *     tags:
+ *       - Surveys
+ *     parameters:
+ *       - in: header
+ *         name: X-Participant-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     responses:
+ *       201:
+ *         description: Survey successfully stored.
+ */
 router.post("/", resolveParticipant, controller.submitSurvey);
+
+/**
+ * @swagger
+ * /surveys:
+ *   get:
+ *     summary: Get previous surveys
+ *     tags:
+ *       - Surveys
+ *     parameters:
+ *       - in: header
+ *         name: X-Participant-Id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       200:
+ *         description: List of completed surveys.
+ */
 router.get("/", resolveParticipant, controller.getSurveys);
 module.exports = router;
